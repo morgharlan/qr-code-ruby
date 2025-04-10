0da2367ed4e90af7c510a6c13096febcda9daccc
morgharlan <morgharlan@gmail.com>
2025-04-10 17:26:02 +0000

    test 1

--- qr.rb
+++ qr.rb
@@ -1,2 +1,11 @@
 # Write your solution here!
-pp "Hello world!"
+require "rqrcode"
+
+# Use the RQRCode::QRCode class to encode some text
+qrcode = RQRCode::QRCode.new("wikipedia qr code")
+
+# Use the .as_png method to create a 500 pixels by 500 pixels image
+png = qrcode.as_png({ :size => 500 })
+
+# Write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
